56afcb9e88e088165e3539a92440ad0143b8b71b
poojakhandale <poojakhandale2000@gmail.com>
2023-04-06 11:31:36 +0530

    json object

--- Employee.js
+++ Employee.js
@@ -24,6 +24,12 @@ function calculateDailyWage(empHrs) {
   return empHrs * WAGE_PER_HR;
 }
 
+const findTotal = (totalVal, dailyVal) => {
+  return totalVal + dailyVal;
+};
+
+let counnt = 0;
+
 let empHrs = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
@@ -37,9 +43,45 @@ while (totalEmpHrs <= MAX_HRS && totalWorkingDays < NUMBER_WORKING_DAYS) {
   let emp_check = Math.floor(Math.random() * 10) % 3;
   empHrs = getWorkingHours(emp_check);
   totalEmpHrs += empHrs;
-  empDailyWageArray.push(calculateDailyWage(empHrs));
-  empMap.set(totalWorkingDays, calculateDailyWage(empHrs));
+  empDailyWageArray.push({
+    dayNum: totalWorkingDays,
+    dayHours: empHrs,
+    dailyWage: calculateDailyWage(empHrs),
+    toString() {
+      return (
+        "\nDay" +
+        this.dayNum +
+        "=> Working hr is " +
+        this.dayHours +
+        "and wage earned: " +
+        this.dailyWage
+      );
+    },
+  });
+
+  empMap.set(totalWorkingDays, empHrs);
 }
+
+let totalHrs = Array.from(empMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArray
+  .filter((dailyWage) => dailyWage > 0)
+  .reduce(findTotal, 0);
+
+console.log("uc9 " + totalHrs + " : " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empMap.forEach((value, key, map) => {
+  if (value == 8) fullWorkingDays.push(key);
+  else if (value == 4) partWorkingDays.push(key);
+  else if (value == 0) nonWorkingDays.push(key);
+});
+
+console.log(fullWorkingDays);
+console.log(partWorkingDays);
+console.log(nonWorkingDays);
 
 console.log(empMap);
 
