f4ca6ac8dcc9e90f51ae117b0cbe537dd6aaef8f
poojakhandale <poojakhandale2000@gmail.com>
2023-04-06 11:26:04 +0530

    Array

--- Employee.js
+++ Employee.js
@@ -1,4 +1,4 @@
-const IS_ABSENT =0;
+const IS_ABSENT = 0;
 
 const Is_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -8,38 +8,37 @@ const WAGE_PER_HR = 20;
 const NUMBER_WORKING_DAYS = 10;
 const MAX_HRS = 100;
 
+function getWorkingHours(emp_check) {
+  switch (emp_check) {
+    case Is_PART_TIME:
+      return PART_TIME_HR;
 
+    case IS_FULL_TIME:
+      return FULL_TIME_HR;
 
-
-
-
-
-function getWorkingHours(emp_check){
-    switch(emp_check){
-        case Is_PART_TIME:
-            return PART_TIME_HR;
-            
-        case IS_FULL_TIME:
-            return FULL_TIME_HR;
-            
-        default:
-            return 0;    
-    }
+    default:
+      return 0;
+  }
+}
+function calculateDailyWage(empHrs) {
+  return empHrs * WAGE_PER_HR;
 }
 
 let empHrs = 0;
 let totalEmpHrs = 0;
-let totalWorkingDays=0;
+let totalWorkingDays = 0;
 
-while(totalEmpHrs<=MAX_HRS && totalWorkingDays<NUMBER_WORKING_DAYS)
-{
-    totalWorkingDays++;
-    let emp_check = Math.floor(Math.random()*10)%3;
-    totalEmpHrs+=getWorkingHours(emp_check);
+let empDailyWageArray = new Array();
+
+while (totalEmpHrs <= MAX_HRS && totalWorkingDays < NUMBER_WORKING_DAYS) {
+  totalWorkingDays++;
+  let emp_check = Math.floor(Math.random() * 10) % 3;
+  empHrs = getWorkingHours(emp_check);
+  totalEmpHrs += empHrs;
+  empDailyWageArray.push(calculateDailyWage(empHrs));
 }
 
+console.log(empDailyWageArray);
 
-
-
-let empWage = totalEmpHrs * WAGE_PER_HR;
-console.log("Wage for the month: "+empWage);
+let empWage = calculateDailyWage(totalEmpHrs);
+console.log("Wage for the month: " + empWage);
